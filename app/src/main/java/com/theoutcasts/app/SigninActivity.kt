4caf8d8a81a38a940b0bbf91449a42a0bdb16777
Rostislav V <sunz89@yandex.ru>
2022-04-20 16:37:48 +0300

    Add SignIn, SignUp activity logic

--- app/src/main/java/com/theoutcasts/app/SigninActivity.kt
+++ app/src/main/java/com/theoutcasts/app/SigninActivity.kt
@@ -1,11 +1,63 @@
 package com.theoutcasts.app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class SigninActivity : AppCompatActivity() {
+    private lateinit var firebaseAuthService: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signin)
+
+        firebaseAuthService = FirebaseAuth.getInstance()
+
+        findViewById<Button>(R.id.signin_btn_login).setOnClickListener() {
+            var email = findViewById<EditText>(R.id.signin_et_email).text.toString().trim()
+            var password = findViewById<EditText>(R.id.signin_et_password).text.toString().trim()
+
+            when {
+                TextUtils.isEmpty(email) -> {
+                    Toast.makeText(
+                        this@SigninActivity,
+                        "Пожалуйста, введите email",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                TextUtils.isEmpty(password) -> {
+                    Toast.makeText(
+                        this@SigninActivity,
+                        "Пожалуйста, введите пароль",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                else -> {
+                    firebaseAuthService.signInWithEmailAndPassword(email, password).addOnCompleteListener() {
+                        if (it.isSuccessful) {
+                            val firebaseUser: FirebaseUser = it.result!!.user!!
+                            val intent = Intent(this@SigninActivity, MainActivity::class.java)
+                            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+                            intent.putExtra("user_id", firebaseUser.uid)
+                            intent.putExtra("email", firebaseUser.email)
+                            startActivity(intent)
+                            finish()
+                        } else {
+                            Toast.makeText(
+                                this@SigninActivity,
+                                "Не удалось авторизоваться",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/theoutcasts/app/SignupActivity.kt
+++ app/src/main/java/com/theoutcasts/app/SignupActivity.kt
@@ -1,11 +1,72 @@
 package com.theoutcasts.app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class SignupActivity : AppCompatActivity() {
+    private lateinit var firebaseAuthService: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
+        firebaseAuthService = FirebaseAuth.getInstance()
+
+        findViewById<Button>(R.id.signup_btn).setOnClickListener() {
+            var email = findViewById<EditText>(R.id.signup_et_email).text.toString().trim()
+            var password = findViewById<EditText>(R.id.signup_et_password).text.toString().trim()
+
+            when {
+                TextUtils.isEmpty(email) -> {
+                    Toast.makeText(
+                        this@SignupActivity,
+                        "Пожалуйста, введите email",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                TextUtils.isEmpty(password) -> {
+                    Toast.makeText(
+                        this@SignupActivity,
+                        "Пожалуйста, введите пароль",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                else -> {
+                    firebaseAuthService.createUserWithEmailAndPassword(email, password).addOnCompleteListener() {
+                        if (it.isSuccessful) {
+                            val firebaseUser: FirebaseUser = it.result!!.user!!
+                            Toast.makeText(
+                                this@SignupActivity,
+                                "Вы зарегистрированы",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            val intent = Intent(this@SignupActivity, SigninActivity::class.java)
+                            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+                            startActivity(intent)
+                            finish()
+                        } else {
+                            Toast.makeText(
+                                this@SignupActivity,
+                                "Произошла ошибка регистрации",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+                }
+            }
+        }
+
+        findViewById<Button>(R.id.signup_btn_already_registered).setOnClickListener() {
+            val intent = Intent(this@SignupActivity, SigninActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
