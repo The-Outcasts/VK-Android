af2380cac92ff1922a948f5a0e9c6656f9f5c645
egor <burcev-egor@mail.ru>
2022-05-22 14:57:00 +0300

    fix

--- app/src/main/java/com/theoutcasts/app/NewPublicationActivity.kt
+++ app/src/main/java/com/theoutcasts/app/NewPublicationActivity.kt
@@ -15,7 +15,9 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.view.drawToBitmap
+import com.theoutcasts.app.data.repository.firebase.UserRepositoryImpl
 import com.theoutcasts.app.domain.interactor.EventInteractor
+import com.theoutcasts.app.domain.interactor.UserInteractor
 import com.theoutcasts.app.domain.model.Event
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
@@ -28,7 +30,9 @@ class NewPublicationActivity : AppCompatActivity() {
     val REQUEST_IMAGE_CAPTURE = 1
     private val image: ImageView by lazy { findViewById(R.id.photoFromCamera) }
     private val publicationDescription: TextView by lazy { findViewById(R.id.description_text) }
+    private val newEvent = Event()
     private val getResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()
+
 
     ) {
         if (it.resultCode == Activity.RESULT_OK) {
@@ -58,18 +62,26 @@ class NewPublicationActivity : AppCompatActivity() {
         val publicationDescription = findViewById<TextView?>(R.id.description_text).text.toString()
         val img = image.drawToBitmap()
         // загрузка в БД, возврат к предыдущему экрану
-        val newEvent = Event()
         newEvent.description = publicationDescription
         newEvent.timeCreated = Date().toString()
-        newEvent.latitude = 1.12
-        newEvent.longitude = 1.13
+        newEvent.latitude = intent.getDoubleExtra("latitude", 1.1)
+        newEvent.longitude = intent.getDoubleExtra("longitude", 1.1)
         newEvent.likeCount = 0
-        newEvent.userId = ""
+        GlobalScope.launch(Dispatchers.IO) {
+            val userManager = UserInteractor(UserRepositoryImpl())
+            val result = userManager.getAuthenticatedUser()
+            result.fold(
+                onSuccess = {
+                    newEvent.userId = it.email
+                },
+                onFailure = {}
+            )
+            //eventsManager.save(newEvent)
+        }
         GlobalScope.launch(Dispatchers.IO) {
             val eventsManager = EventInteractor(eventRepository = EventRepositoryImpl1())
             //eventsManager.save(newEvent)
         }
         Toast.makeText(this, newEvent.timeCreated, Toast.LENGTH_LONG).show()
     }
-
 }
